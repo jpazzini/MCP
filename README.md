e7cba125d0fe2161ea8f7abcaa7beeb3bac3e257
jpazzini <Jacopo.Pazzini@cern.ch>
2024-10-03 17:14:47 +0200

    cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-## First draft of the material for the course _Modern Computing for Physics_ 
-### M.Sc. in Physic of Data — University of Padova — Academic Year 2024-2025
--- numba/hello.py
+++
@@ -1,9 +0,0 @@
-from numba import cuda
-
-# check if any cuda gpu is detected
-print(cuda.gpus)
-
-# select the device_id to assign it to numba
-cuda.select_device(0)
-
-print('all done')
